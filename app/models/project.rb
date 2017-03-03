688406ce84074be6c99f3f009f139d928c20ff6c
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2017-03-03 14:52:08 -0800

    create a user admin account and add corresponding routes in navbar for access to admin panel. additional styling

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,2 @@
 class Project
-  def get_projects
-   response = HTTParty.get("https://api.github.com/users/haleyswain/repos")
-   response
-  end
 end
--- app/services/get_project.rb
+++ app/services/get_project.rb
@@ -1,6 +1,6 @@
 class Project
   def get_projects
-   response = HTTParty.get("https://api.github.com/users/haleyswain/repos")
+   response = HTTParty.get("https://api.github.com/user/starred?access_token=edc97923603e21b3a01718ffa4fd819164dfacce")
    response
   end
 end
